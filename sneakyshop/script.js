7078a9b92ae825e0e4cc25968518821c24425c76
Endlessvoid303 <andre080608@gmail.com>
2024-02-29 20:08:52 +0100

    test

--- sneakyshop/script.js
+++ sneakyshop/script.js
@@ -1,4 +1,8 @@
 const webhookUrl = 'https://discord.com/api/webhooks/1212764499157712927/HR5H26bp09yscH6divAzSPIAI1dzkv6uItkvANdzQJKMBa6p_QcAfeZ7pqepGLtx7Mn-';
+const addButton = document.querySelector('.add-button');
+const removeButton = document.querySelector('.remove-button');
+const itemAmount = document.querySelector('.current-amount');
+const form = document.querySelector('#shopform');
 function sendMessage() {
     const username = encodeURIComponent(document.getElementById('username').value);
     const order = encodeURIComponent(document.getElementById('order').value);
@@ -22,4 +26,25 @@ function sendMessage() {
       console.error('Error sending message to Discord:', error);
       // Show an error message, etc.
     });
+}
+addButton.addEventListener('click', () => {
+  let amount = parseInt(itemAmount.innerText);
+  amount++;
+  itemAmount.innerText = amount;
+  updateFormPrice(form, 10.00);
+});
+
+removeButton.addEventListener('click', () => {
+  let amount = parseInt(itemAmount.innerText);
+  if (amount > 0) {
+    amount--;
+    itemAmount.innerText = amount;
+    updateFormPrice(form, -10.00);
+  }
+});
+
+function updateFormPrice(form, priceChange) {
+  let currentPrice = parseFloat(form.elements['total-prices'].value);
+  currentPrice += priceChange;
+  form.elements['total-prices'].value = currentPrice.toFixed(2);
 }
